94fc42c1f7f0af82b26b65130f8918d2021cbb2f
Maxawa <64471075+Maxawa@users.noreply.github.com>
2023-05-18 17:39:45 +0200

    cant see any rgb but this is just the camera
    yay now you can see your camera
    doesnt work on mobile for some god damn reason

---
+++ public/Index.js
@@ -0,0 +1,30 @@
+const videoThing = document.getElementById('videoThing');
+const options = document.getElementById('list');
+const debug = document.getElementById('debugtext');
+ShowDevice();
+navigator.mediaDevices.enumerateDevices()
+  .then(function(devices) {
+    const cameras = devices.filter(device => device.kind === 'videoinput');
+    cameras.forEach(function(camera) {
+      console.log(cameras);
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.text = 'Camera ' + (options.length + 1);
+      options.appendChild(option);
+    });
+
+    options.addEventListener('change', ShowDevice);
+  })
+  .catch(function(error) {
+    console.error('Error enumerating devices:', error);
+  });
+function ShowDevice(){
+    const usedCamera = options.value;
+    navigator.mediaDevices.getUserMedia({ video: { deviceId: usedCamera } })
+    .then(function(stream) {
+     videoThing.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error('Error accessing camera:', error);
+      });
+}
