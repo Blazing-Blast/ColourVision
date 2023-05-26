bad0aeeebdae496e333315f8677ffe87c0a35d40
Blazing-Blast <69185984+Blazing-Blast@users.noreply.github.com>
2023-05-26 10:33:39 +0200

    Set logged pixel to purple

--- public/Index.js
+++ public/Index.js
@@ -45,11 +45,14 @@ videoThing.addEventListener('play', function () {
     (function loop() {
         ctx.drawImage($this, 0,0, canvas.width, canvas.height);
         setTimeout(loop, frameTime); // drawing at 30fps
-        const rgbData = ctx.getImageData(0,0,canvas.width, canvas.height).data;
+        const rgbData = ctx.getImageData(0,0,canvas.width, canvas.height).data; // x = 0; y = 0; w = width; h = height;
         const middle = 4*(width * (height/2 + 1));
         const r = rgbData[middle];
         const g = rgbData[middle + 1];
         const b = rgbData[middle + 2];
         console.log("R: " + r + ", G: " + g + ", B: " + b);
+        rgbData[middle + 1] = 255;
+        rgbData[middle + 1] = 0;
+        rgbData[middle + 2] = 200;
     })();
 }, 0);
